08d6f190730008940dff083e40a6fbb5ca6c0ae7
lufire <lukas.feierabend@gmail.com>
2020-06-29 20:14:23 +0200

    renaming

--- GUI_function.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-
-def callback():                     #fetches current entry text
-    print(E1.get())
-    print(E2.get())
-    print(E3.get())
-    print(E4.get())
---
+++ gui_function.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+def callback():                     #fetches current entry text
+    print(entry1.get())
+    print(entry2.get())
+    print(entry3.get())
+    print(entry4.get())
