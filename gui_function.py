b5d1d3477a9f2a639a1b21ad18bdc745af354a93
lufire <lukas.feierabend@gmail.com>
2020-06-30 14:39:35 +0200

    deleted unused file

--- gui_function.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-
-def callback():                     #fetches current entry text
-    print(entry1.get())
-    print(entry2.get())
-    print(entry3.get())
-    print(entry4.get())
